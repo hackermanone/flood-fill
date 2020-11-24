d7edee369adead3ae447ba3278f90293d0b96fea
Ricky Chen <Ricky.chen98@hotmail.com>
2020-11-24 11:34:10 -0500

    Create grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cellSize, setcellSize] = useState(20);
+  const [numRows, setNumRows] = useState(30);
+  const [numCols, setNumCols] = useState(40);
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    // populate the subarrays with a tuple [filled : 1|0, color: hexcode]
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array(numCols).fill([0, '#fff']))
+    }
+
+    return rows;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="grid" style={{
+     gridTemplateColumns: `repeat(${numCols}, ${cellSize}px)`,
+     gridTemplateRows: `repeat(${numRows}, ${cellSize}px)`,
+   }}>
+    {grid.map((rows, i) => {
+        return rows.map((cols, j) => {
+          return <div 
+                  key={`${i}-${j}`}
+                  style={{
+                    backgroundColor: grid[i][j][1],
+                    border: "solid 1px black"
+                  }}
+                  />
+        })
+      })
+    }
+   </div>
   );
 }
 
